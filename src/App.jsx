009daf25575a7d7889f9a16193c3c47a56fccd0d
kritika <kritikabhardwaj2513@gmail.com>
2025-05-09 22:35:35 +0530

    add filter

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Card from "./Card";
 export default function App(){
   return(
     <>
+    
     <Card/>
     </>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,21 +1,35 @@
-
+import { useState } from 'react'
 import {tasksData} from './data'
 import Button from './Button'
-import { useState } from 'react'
 
 export default function Card(){
 
-    const [index,setIndex] = useState[0];
-    function handleClick(){
-        setIndex(index +1)
+
+    const filteredData = tasksData.filter((data)=>{
+    filteredData = 
+    })
+    const [index,setIndex] = useState(0);
+    const [fiter, setFilter] = useState(0);
+
+    function handleNextClick(){
+    setIndex((index) => (index + 1) % filteredData.length);
+    }
+    function handlePreviousClick(){
+        setIndex((index) =>
+            (index - 1 + filteredData.length) % filteredData.length
+        );
     }
 
 return(
 <>
-    <Button classGiven="nextbutton" label="Next" onSmash={handleClick}/>
+    <Button classGiven="nextbutton" label="Next" onSmash={handleNextClick}/>
+    <Button classGiven="previousbutton" label="PREVIOUS" onSmash={handlePreviousClick}/>
+    <Button classGiven="filterbutton" label="FILTER ALL()" onSmash={handlePreviousClick}/>
+
+
 <div>
-    <h3>{tasksData[index].text}</h3>
-    <p>{tasksData[index].status}</p>
+    <h3>{filteredData[index].text}</h3>
+    <p>{filteredData[index].status}</p>
 </div>
 </>
 )
