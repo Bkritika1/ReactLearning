9176c2f82c80651abfb100d88706a03f9ac3d9a7
kritika <kritikabhardwaj2513@gmail.com>
2025-05-09 22:49:03 +0530

    filterworks

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import {tasksData} from './data'
 import Button from './Button'
 
+
+const FILTERS = ["all", "incomplete", "in-progress", "completed"];
 export default function Card(){
-
-
-    const filteredData = tasksData.filter((data)=>{
-    filteredData = 
-    })
     const [index,setIndex] = useState(0);
-    const [fiter, setFilter] = useState(0);
+    const [filter, setFilter] = useState(0);
 
+    const filteredData = tasksData.filter((task)=>{
+       if(FILTERS[filter]=="all"){
+            return true;
+        }
+        return task.status==FILTERS[filter];
+    })
     function handleNextClick(){
     setIndex((index) => (index + 1) % filteredData.length);
     }
@@ -19,12 +22,21 @@ export default function Card(){
             (index - 1 + filteredData.length) % filteredData.length
         );
     }
+function handleToggleClick(){
+        setFilter(
+            (f)=>(f+1)%FILTERS.length
+        );
+        setIndex(0);
+    }
+
+
+
 
 return(
 <>
     <Button classGiven="nextbutton" label="Next" onSmash={handleNextClick}/>
     <Button classGiven="previousbutton" label="PREVIOUS" onSmash={handlePreviousClick}/>
-    <Button classGiven="filterbutton" label="FILTER ALL()" onSmash={handlePreviousClick}/>
+    <Button classGiven="filterbutton" label={`Toggle: ${FILTERS[filter]}`} onSmash={handleToggleClick}/>
 
 
 <div>
